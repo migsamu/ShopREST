8860596f76483ca8b95a95a4002b6226000ebb07
Miguel <mufrikiler@gmail.com>
2021-11-24 00:56:07 +0100

    muerte

--- src/main/java/org/iesfm/shop/dao/jdbc/JDBCArticlesDAO.java
+++ src/main/java/org/iesfm/shop/dao/jdbc/JDBCArticlesDAO.java
@@ -2,6 +2,8 @@ package org.iesfm.shop.dao.jdbc;
 
 import org.iesfm.shop.Article;
 import org.iesfm.shop.dao.ArticleDAO;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
@@ -10,11 +12,13 @@ import java.util.*;
 
 public class JDBCArticlesDAO implements ArticleDAO {
 
+    private static final String INSERT_ARTICLE = "INSERT INTO Article (id, name, price) " +
+            "VALUES (:id, :name, :price)";
     private static final String SELECT_ARTICLES = "SELECT * FROM Article ";
+    private static final String SELECT_ARTICLE_BY_ID = "SELECT * FROM Article WHERE id=:id";
     private static final String SELECT_ARTICLES_BY_ARTICLES_ID = "SELECT name FROM Tag where article_id = :article_id ";
     private static final String SELECT_ARTICLES_BY_TAG =
-            "SELECT a. * " +
-                    "FROM Article a ";
+            "SELECT article_id FROM Tag WHERE name = :tag";
 
     private final RowMapper<Article> ARTICLE_ROW_MAPPER =
             ((rs, rowNum) -> new Article(
@@ -43,26 +47,49 @@ public class JDBCArticlesDAO implements ArticleDAO {
         Map<String, Object> params = new HashMap<>();
         params.put("article_id", articleId);
 
-        return new HashSet<>(jdbc.query(SELECT_ARTICLES_BY_ARTICLES_ID, params, (rs, n) ->
-                rs.getString("name")));
+        return new HashSet<String>(jdbc.query(
+                SELECT_ARTICLES_BY_ARTICLES_ID,
+                params,
+                (rs, n) ->
+                        rs.getString("name")
+        ));
 
     }
 
-    private List<Article> articlesByTag(String tag) {
+    private List<Integer> articlesByTag(String tag) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("tag", tag);
 
-        return null;
+        return jdbc.query(
+                SELECT_ARTICLES_BY_ARTICLES_ID,
+                params,
+                (rs, n) ->
+                        rs.getInt("article_id"));
     }
 
     @Override
     public List<Article> list(String tag) {
-
-
-        return null;
+        Map<String, Object> params = new HashMap<>();
+        List<Article> articles = new ArrayList<>();
+        for (int id : articlesByTag(tag)) {
+            params.put("id", id);
+            jdbc.query(
+                    SELECT_ARTICLES,
+                    params,
+                    ARTICLE_ROW_MAPPER);
+        }
+        return articles;
     }
 
     @Override
     public Article get(int id) {
-        return null;
+        Map<String, Object> params = new HashMap<>();
+        params.put("id", id);
+        try {
+            return jdbc.queryForObject(SELECT_ARTICLE_BY_ID, params, ARTICLE_ROW_MAPPER);
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
     }
 
     @Override
